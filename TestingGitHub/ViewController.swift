e56537052ee5369d0f14eb6954a3eea0611fd8be
Htun Min Latt <htunminlatt.iosdev@gmail.com>
2019-05-23 10:47:03 +0630

    Initial Commit

---
+++ TestingGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingGitHub
+//
+//  Created by ALAY on 5/23/19.
+//  Copyright © 2019 Aerial-Yangon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
